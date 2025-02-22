92cf5d9c10e1f44292633f7a29477f0dc646ec66
刘亚泉 <liu15107128281@gmail.com>
2025-02-23 04:13:42 +0800

    修改

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <p>1</p>
+      { children }
       </body>
     </html>
   );
